3df1af3547b1a510158606fd45b540d26abc48d8
mfagan1 <35314131+mfagan1@users.noreply.github.com>
2019-05-28 23:34:59 -0500

    Update LocalCollection.swift

--- ZwaxMessenger/ZwaxMessenger/LocalCollection.swift
+++ ZwaxMessenger/ZwaxMessenger/LocalCollection.swift
@@ -1,10 +1,7 @@
 //
 //  LocalCollection.swift
 //  ZwaxMessenger
-//
-//  Created by Kevin Hoffmeister on 3/11/19.
-//  Copyright © 2019 Ryden Neal. All rights reserved.
-//
+
 
 import Foundation
 import FirebaseFirestore
